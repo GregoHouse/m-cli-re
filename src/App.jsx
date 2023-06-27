ad53fbd01d42954b874d37cd7b04f92c5178c987
LucianoGimenezz <luciano.gimenezz21@gmail.com>
2023-06-27 10:54:23 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <main className="w-full h-full flex flex-col items-center justify-center">
+      <h1 className="text-6xl text-white font-semibold tracking-tight">Hello world</h1>
+    </main>
+  )
+}
+
+export default App
