8907413115a6d9a3aaa9a45006142f5aa6cfeb42
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2023-07-10 11:03:26 -0500

    feat: add protection to routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,12 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { LoginPage } from './pages/';
+import { Provider } from 'react-redux';
+import { store } from './store/';
+import Router from './router';
 
 function App() {
 	return (
-		// <main className="w-full h-full flex flex-col items-center justify-center bg-[#203144]">
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<LoginPage />} />
-
-				<Route
-					path="/dashboard"
-					element={
-						<h1 className="text-6xl text-white font-semibold tracking-tight">
-							dashboard
-						</h1>
-					}
-				/>
-
-				<Route
-					path="*"
-					element={
-						<h1 className="text-6xl text-white font-semibold tracking-tight">
-							404
-						</h1>
-					}
-				/>
-			</Routes>
-		</BrowserRouter>
-		// </main>
+		<Provider store={store}>
+			<Router />;
+		</Provider>
 	);
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,5 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import { store } from './store/'
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
