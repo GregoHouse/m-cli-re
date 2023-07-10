cfab3420549e94a340b271f5f12d4c95ba08229b
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2023-07-10 16:00:52 -0500

    fix: use a better alert in login error

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -3,13 +3,17 @@ import { loginUser } from '../../services/auth';
 import FormLogin from './FormLogin';
 import { userActions } from '../../store/user';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function LoginPage() {
+	const [loginError, setLoginError] = useState(null);
+
 	const dispatch = useDispatch();
 	const navigation = useNavigate();
 	const onSubmit = userData => {
 		loginUser(userData)
 			.then(token => {
+				console.log('token');
 				dispatch(
 					userActions.setUserInfo({
 						isLogged: true,
@@ -22,7 +26,10 @@ export default function LoginPage() {
 				navigation('/dashboard');
 			})
 			.catch(e => {
-				alert('Incorrect credentials');
+				setLoginError('Incorrect credentials');
+				setTimeout(() => {
+					setLoginError(null);
+				}, 2000);
 				console.error(e);
 			});
 	};
@@ -32,7 +39,14 @@ export default function LoginPage() {
 			<header className="w-[300px] my-[-30px] mx-[-10px]">
 				<img src="/images/matching.jpg" className="h-[150px]" />
 			</header>
-			<div className="flex-1 flex justify-center items-center mt-[-100px]">
+			<div className="flex-1 flex  flex-col gap-4 justify-center items-center mt-[-100px]">
+				{loginError ? (
+					<div className="py-2 px-4 bg-red-400 text-white front-semibold rounded-md">
+						{loginError}
+					</div>
+				) : (
+					<></>
+				)}
 				<FormLogin onSubmit={onSubmit} />
 			</div>
 		</main>
