e175ddda51aaf7a13bb170fb067010ee0147092a
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2023-07-12 16:35:48 -0500

    feat: add env variables

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { ENV } from '../constants/env';
 
 export const loginUser = async userData => {
-	const url = 'https://m-ser.onrender.com/login';
+	const url = `${ENV.SERVER_URL_BASE}/users/login`;
 	const res = await axios.post(url, userData);
 	return res.data;
 };
